7f64a7c3e89374d92620a54fa5a2afd2d855d030
Dustin Scott <dscott@redhat.com>
2018-08-07 14:54:05 -0500

    updated vm_name to most recent tested version

--- CloudForms_Integrations/Common/Naming.class/__methods__/vm_name.rb
+++ CloudForms_Integrations/Common/Naming.class/__methods__/vm_name.rb
@@ -1,12 +1,11 @@
 #
-# Description: Creates System Name with following format:
+# Description: Creates System Name based on dialog user inputs.
 #
 # Author:      : Dustin Scott, Red Hat
 # Creation Date: 9-June-2017
 #
 # NOTES:
-#   - Number of integers to use for Series is set via the SERIES_COUNT constant
-#   - Assumes downcase of System Names
+#   - Number of integers to use for Series is set via the NAMING_DIGITS constant
 #
 
 # ====================================
@@ -18,17 +17,24 @@
 @org    = $evm.root['tenant'].name
 @debug  = $evm.root['debug'] || false
 
-# set method constants
-SUPER_PREFIX       = 'dialog'
-TAG_PREFIX         = 'tag'
-OPTION_PREFIX      = 'option'
-ARRAY_PREFIX       = 'Array::'
-PASS_PREFIX        = 'Password::'
-TAG_CONTROL_PREFIX = 'Classification::'
+#
+# potential dialog prefix (e.g. PREFIX_my_option)
+# potential prefixes dialog items (e.g. PREFIX_SUBPREFIX_my_option)
+#
+DIALOG_PREFIX    = 'dialog'
+TAG_SUBPREFIX    = 'tag'
+OPTION_SUBPREFIX = 'option'
+
+#
+# potential value prefixes
+#
+ARRAY_VALUE_PREFIX       = 'Array::'
+PASS_VALUE_PREFIX        = 'Password::'
+TAG_CONTROL_VALUE_PREFIX = 'Classification::'
 
 #
 # ORDERED_NAMING_VALUES:
-# The values, in order, to be placed in the naming sequence.
+#   The values, in order, to be placed in the naming sequence.
 #
 # Hash Keys: Logging purposes. Names to denote in logs which values are associated.
 #
@@ -36,19 +42,41 @@ TAG_CONTROL_PREFIX = 'Classification::'
 #   Symbols: pulled via options or tags
 #   Strings: literal
 #
-# SERIES_COUNT:
-# An instance number (e.g. 01, o2, 03) to be placed at the end of ORDERED_NAMING_VALUES
+# NAMING_DIGITS:
+#   The number of digits (e.g. 01, 02, 03 = 2 AND 001, 002, 003 = 3) to be placed at the end of ORDERED_NAMING_VALUES
 #
 ORDERED_NAMING_VALUES = {
-  :group_prefix => ($evm.root['user'].current_group.tags('group_prefix').first.downcase rescue nil), # 1 - group prefix tag on group
-  :separator1   => '-',                                                                              # 2 - separator
-  :system_type  => :sn_system_type,                                                                  # 3 - system type
-  :separator2   => '-',                                                                              # 4 - separator
-  :platform     => ($evm.root['miq_provision'].source.platform.first.downcase rescue nil),           # 5 - first character from template platform
-  :form_factor  => :sn_form_factor,                                                                  # 6 - form factor
-  :environment  => :sn_environment                                                                   # 7 - environment
+  :application => :sn_application,                                                        # 1 - application being provisioned
+  :environment => :sn_environment,                                                        # 2 - environment provisioning to
+  :platform    => ($evm.root['miq_provision'].source.platform.first.downcase rescue nil), # 3 - first character from template platform
+  :location    => :sn_location,                                                           # 4 - the form factor of the system
+  :form_factor => :sn_form_factor                                                         # 5 - form factor
 }.freeze
-SERIES_COUNT = 2
+NAMING_DIGITS         = 3
+
+#
+# Optional, set non critical values and their defaults if missing
+#
+NON_CRITICAL_VALUES   = {
+  :environment => 'n'
+}
+
+#
+# caps lock
+#   DOWNCASE:
+#     true:  forces downcase of the VM name
+#     false: raw VM names from derived values
+#
+#   UPCASE_WINDOWS:
+#     true:  forces windows to all upper case
+#     false: does not modify windows system names
+#
+#   NOTE:
+#     UPCASE_WINDOWS will override the DOWNCASE option in the case that the system provisioned is windows
+#     as determined from the provisioning object.
+#
+DOWNCASE       = true
+UPCASE_WINDOWS = false
 
 # ====================================
 # define methods
@@ -81,12 +109,12 @@ end
 # try intrusive methods to get a value
 # TODO: untested past tag_0, option_0, etc.  Likely will need updated.
 def get_value_intrusive(option_prefix, option)
-  full_prefix = SUPER_PREFIX + '_' + option_prefix
+  full_prefix = DIALOG_PREFIX + '_' + option_prefix
   0.upto(9) do |build_num|
-    @value   = get_symbol_or_string_value((full_prefix + '_' + build_num.to_s + '_' + option.to_s))                # least intrusive
-    @value ||= get_symbol_or_string_value((SUPER_PREFIX + '_' + option.to_s))                                      # slightly more intrusive
-    @value ||= get_symbol_or_string_value((ARRAY_PREFIX + full_prefix + '_' + build_num.to_s + '_' + option.to_s)) # most intrusive (tag control array)
-    @value ||= get_symbol_or_string_value((PASS_PREFIX + full_prefix + '_' + build_num.to_s + '_' + option.to_s))  # most intrusive (password)
+    @value   = get_symbol_or_string_value((full_prefix + '_' + build_num.to_s + '_' + option.to_s))                      # least intrusive
+    @value ||= get_symbol_or_string_value((DIALOG_PREFIX + '_' + option.to_s))                                           # slightly more intrusive
+    @value ||= get_symbol_or_string_value((ARRAY_VALUE_PREFIX + full_prefix + '_' + build_num.to_s + '_' + option.to_s)) # most intrusive (tag control array)
+    @value ||= get_symbol_or_string_value((PASS_VALUE_PREFIX + full_prefix + '_' + build_num.to_s + '_' + option.to_s))  # most intrusive (password)
 
     break if @value # break the loop if you have a value
   end
@@ -96,8 +124,8 @@ end
 # get tag value
 def get_tag_value(prov, tag)
   log(:info, "get_tag_value: Parsing dialog options to attempt to get tag value for option: <#{tag}>")
-  value   = prov.get_tags[tag.to_sym]            # try standard methods
-  value ||= get_value_intrusive(TAG_PREFIX, tag) # try intrusive methods
+  value   = prov.get_tags[tag.to_sym]               # try standard methods
+  value ||= get_value_intrusive(TAG_SUBPREFIX, tag) # try intrusive methods
 
   log(:info, "get_tag_value: Returning value: <#{value}> for tag: <#{tag}>")
   return value
@@ -106,44 +134,38 @@ end
 # get option value
 def get_option_value(prov, option)
   log(:info, "get_option_value: Parsing dialog options to attempt to get value for option: <#{option}>")
-  value   = prov.get_option(option.to_sym)                                                             # try standard methods
+  value   = prov.get_option(option.to_sym) || prov.get_option(option.to_s)                             # try standard methods
   value ||= @dialog_options[option.to_sym] || @dialog_options[option.to_s] unless @dialog_options.nil? # try standard methods if we have @dialog_options
   value ||= @ws_values[option.to_sym]      || @ws_values[option.to_s]      unless @ws_values.nil?      # try standard methods if we have @ws_values
-  value ||= get_value_intrusive(OPTION_PREFIX, option)                                                 # try intrusive methods
+  value ||= get_value_intrusive(OPTION_SUBPREFIX, option)                                              # try intrusive methods
 
   log(:info, "get_option_value: Returning value: <#{value}> for option: <#{option}>")
   return value
 end
 
-# get provisioning option
-def get_prov_option(prov, option)
-  log(:info, "get_prov_option: Getting prov option: <#{option}>")
+# get value from provisioning object
+def get_prov_value(prov, option_or_tag)
+  log(:info, "get_prov_value: Getting value for option_or_tag: <#{option_or_tag}>")
 
-  # get the dialog options and attempt to return a valid value
-  @dialog_options = get_dialog_options(prov)    unless @dialog_options
-  @ws_values      = prov.get_option(:ws_values) unless @ws_values
-  value           = get_tag_value(prov, option) || get_option_value(prov, option)
+  # attempt to return a valid value
+  value = get_tag_value(prov, option_or_tag) || get_option_value(prov, option_or_tag)
 
   # find the classification name if we used a tag control
-  if value.nil?
-    return value
-  else
-    if value.include?(TAG_CONTROL_PREFIX)
-      tag_object = $evm.vmdb(:classification).find_by_id(value.split('::').last)
+  if value.to_s.include?(TAG_CONTROL_VALUE_PREFIX)
+    tag_object = $evm.vmdb(:classification).find_by_id(value.split('::').last)
 
-      if tag_object.nil?
-        sanitized_value = nil
-      else
-        sanitized_value = tag_object.name
-      end
-
-      log(:info, "get_prov_option: Returning prov value: <#{sanitized_value}> for option: <#{option}>")
-      return sanitized_value
+    if tag_object.nil?
+      sanitized_value = nil
     else
-      log(:info, "get_prov_option: Returning prov value: <#{value}> for option: <#{option}>")
-      return value
+      sanitized_value = tag_object.name
     end
+  else
+    sanitized_value = value
   end
+
+  # log and return the proper value
+  log(:info, "get_prov_value: Returning prov value: <#{sanitized_value}> for option_or_tag: <#{option_or_tag}>")
+  return sanitized_value
 end
 
 # updates the naming_values array
@@ -153,24 +175,49 @@ def update_naming_values(name, value)
   @naming_values.push(value)
 end
 
-# set derived_name
-def set_derived_name(prov)
+# sets the name based on our case selections
+def get_name_with_case(vm_name, prov)
+  log(:info, "get_name_with_case: Getting proper character capitalization for vm_name: <#{vm_name}>")
+
+  # adjust the vm name appropriately
+  adjusted_vm_name = vm_name
+  adjusted_vm_name = vm_name.downcase if DOWNCASE
+  adjusted_vm_name = vm_name.upcase   if UPCASE_WINDOWS && prov.try(:source).try(:platform).try(:first).try(:downcase) == 'windows'
+
+  log(:info, "get_name_with_case: Returning adjusted_vm_name: <#{adjusted_vm_name}>")
+  return adjusted_vm_name
+end
+
+# get the derived name
+def get_derived_name(prov)
   begin
-    log(:info, "set_derived_name: Inspecting ORDERED_NAMING_VALUES: #{ORDERED_NAMING_VALUES.inspect}") if @debug
+    log(:info, "get_derived_name: Inspecting ORDERED_NAMING_VALUES: #{ORDERED_NAMING_VALUES.inspect}")
+
+    # get dialog options and ws_values
+    @dialog_options = get_dialog_options(prov)
+    @ws_values      = prov.get_option(:ws_values)
 
     # log and check each value and push to naming values array
     @naming_values = []
     ORDERED_NAMING_VALUES.each do |name, value|
       if value.nil?
-        raise "Unable to find critical element <#{name}> in naming VM"
+        if NON_CRITICAL_VALUES[name].nil?
+          raise "Unable to find critical element <#{name}> in naming VM"
+        else
+          update_naming_values(name, NON_CRITICAL_VALUES[name])
+        end
       elsif value.is_a?(String)
         update_naming_values(name, value)
       elsif value.is_a?(Symbol)
-        log(:info, "set_derived_name: Value <#{value}> for <#{name}> is a Symbol.  Attempting to pull via options and tags.")
-        dialog_value = get_prov_option(prov, value)
+        log(:info, "get_derived_name: Value <#{value}> for <#{name}> is a Symbol.  Attempting to pull via options and tags.")
+        dialog_value = get_prov_value(prov, value)
 
         if dialog_value.nil?
-          raise "Unable to find dialog value for <#{value}>"
+          if NON_CRITICAL_VALUES[name].nil?
+            raise "Unable to find dialog_value for <#{value}>"
+          else
+            update_naming_values(name, NON_CRITICAL_VALUES[name])
+          end
         else
           update_naming_values(name, dialog_value)
         end
@@ -180,25 +227,24 @@ def set_derived_name(prov)
     end
 
     # set the derived name
-    derived_name = nil
     if $evm.object['vm_prefix']
-      derived_name = "#{$evm.object['vm_prefix']}#{@naming_values.join}".downcase
+      derived_name = get_name_with_case("#{$evm.object['vm_prefix']}#{@naming_values.join}", prov)
     else
-      derived_name = @naming_values.join.downcase
+      derived_name = get_name_with_case(@naming_values.join, prov)
     end
 
     # log and return the derived vm name
-    log(:info, "Derived VM Name: <#{derived_name}>")
-    return derived_name + "$n{#{SERIES_COUNT}}"
+    log(:info, "get_derived_name: Derived a VM Name: <#{derived_name}> + NAMING_DIGITS <#{NAMING_DIGITS}>")
+    return derived_name + "$n{#{NAMING_DIGITS}}"
   rescue => err
-    log(:error, "set_derived_name: #{err}")
+    log(:error, "get_derived_name: #{err}")
     return nil
   end
 end
 
 # log final name and update it on the object
 def update_vm_name(name, prov)
-  log(:info, "VM Name: <#{name}>")
+  log(:info, "update_vm_name: VM Name: <#{name}>")
   $evm.object['vmname'] = name
 end
 
@@ -207,14 +253,12 @@ end
 # ====================================
 
 begin
-  # log entering method and dump root/object attributes
-  $evm.instantiate('/Common/Log/LogBookend' + '?' + { :bookend_status => :enter, :bookend_parent_method => @method, :bookend_org => @org }.to_query)
-  [ 'root', 'object' ].each { |object_type| $evm.instantiate("/Common/Log/DumpAttrs?object_type=#{object_type}") } if @debug
-
   # ensure we are using this method in the proper context
   case $evm.root['vmdb_object_type']
-    when 'miq_provision', 'miq_provision_request', 'miq_provision_request_template'
+    when 'miq_provision', 'miq_provision_request'
       prov = $evm.root['miq_provision_request'] || $evm.root['miq_provision'] || $evm.root['miq_provision_request_template']
+    when 'miq_provision_request_template'
+      exit MIQ_OK
     else
       raise "Invalid $evm.root['vmdb_object_type']: #{$evm.root['vmdb_object_type']}"
   end
@@ -229,13 +273,13 @@ begin
     log(:info, "current_vm_name from dialog: <#{current_vm_name}>; vms_requested from dialog: <#{vms_requested}>")
 
     # no vm name chosen from dialog, or changeme requested
-    if current_vm_name.blank? || current_vm_name == 'changeme'
-      derived_name = set_derived_name(prov)
+    if current_vm_name.blank? || current_vm_name.nil? || current_vm_name == 'changeme'
+      derived_name = get_derived_name(prov)
     else
       if vms_requested == 1
         derived_name = current_vm_name
       else
-        derived_name = "#{current_vm_name}$n{#{SERIES_COUNT}}"
+        derived_name = "#{current_vm_name}$n{#{NAMING_DIGITS}}"
       end
     end
 
@@ -254,7 +298,6 @@ begin
   # ====================================
 
   # log exiting method and exit with MIQ_OK status
-  $evm.instantiate('/Common/Log/LogBookend' + '?' + { :bookend_status => :exit, :bookend_parent_method => @method, :bookend_org => @org }.to_query)
   exit MIQ_OK
 
 # set ruby rescue behavior
@@ -281,6 +324,5 @@ rescue => err
   end
 
   # log exiting method
-  $evm.instantiate('/Common/Log/LogBookend' + '?' + { :bookend_status => :exit, :bookend_parent_method => @method, :bookend_org => @org }.to_query)
-  exit MIQ_WARN
+  exit MIQ_ABORT
 end
